b0fb73641e9b708e04840e923a1f9ad865650096
Infant Frontender <newbornfrontender@gmail.com>
2018-11-27 11:25:40 +0300

    Добавлены компоненты-страницы

---
+++ components/my-view-about.js
@@ -0,0 +1,24 @@
+import { updateMetadata } from 'pwa-helpers/metadata';
+
+const template = document.createElement('template');
+
+template.innerHTML = `
+<h1>About</h1>
+`;
+
+class MyViewAbout extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    updateMetadata({
+      title: 'About',
+    });
+  }
+}
+
+customElements.define('my-view-about', MyViewAbout);
---
+++ components/my-view-home.js
@@ -0,0 +1,24 @@
+import { updateMetadata } from 'pwa-helpers/metadata';
+
+const template = document.createElement('template');
+
+template.innerHTML = `
+<h1>Home</h1>
+`;
+
+class MyViewHome extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    updateMetadata({
+      title: 'Home',
+    });
+  }
+}
+
+customElements.define('my-view-home', MyViewHome);
